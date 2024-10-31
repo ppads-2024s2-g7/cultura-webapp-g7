af973847342304ae37e356f48bad625ba0b7e72e
NTNGLTT <122834963+NTNGLTT@users.noreply.github.com>
2024-10-31 23:56:05 +0000

    NatanSubclasse

--- demo/src/main/java/com/example/demo/DesenhoRepo.java
+++ demo/src/main/java/com/example/demo/DesenhoRepo.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
+
+
 @Repository
 public interface DesenhoRepo extends JpaRepository<Desenho, Long> {
 
